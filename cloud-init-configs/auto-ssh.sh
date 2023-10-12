cdd017337950115ec25fc465c17a4d7f529910c0
BRReed <b.r.reed@gmail.com>
2023-10-11 20:08:38 -0700

    changed ssh connection script to be more functional
    Signed-off-by: BRReed <b.r.reed@gmail.com>

--- cloud-init-configs/auto-ssh.sh
+++ cloud-init-configs/auto-ssh.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
 USER="$1"
-VM_NAME="$2"
+VMNAME="$2"
 
-ip_addr=$(virsh domifaddr "$VM_NAME" | \
-          grep -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' | \
-          awk '{print $1}'\
-        )
-
-echo "$VM_NAME : $ip_addr"
-
-ssh "$USER"@"$ip_addr"
+ssh -t -o StrictHostKeyChecking=no $USER@$(virsh domifaddr $VMNAME | \
+                                           awk -F'[ /]+' '{if (NR>2) print $5}')
