1103e7de9f061705266ad6b65508d484bb66b5c3
BRReed <b.r.reed@gmail.com>
2023-10-12 20:42:57 -0700

    removing a break in a line that's longer than 79 characters. this makes me want to die
    Signed-off-by: BRReed <b.r.reed@gmail.com>

--- cloud-init-configs/auto-ssh.sh
+++ cloud-init-configs/auto-ssh.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 # arg $1=username; arg $2=vm name
-ssh -t -o StrictHostKeyChecking=no $1@$(virsh domifaddr $2 | \
-                                           awk -F'[ /]+' '{if (NR>2) print $5}')
+ssh -t -o StrictHostKeyChecking=no $1@$(virsh domifaddr $2 | awk -F'[ /]+' '{if (NR>2) print $5}')
